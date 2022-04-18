f7d3d1941ef6b6e39f6258948e9f24f7d19ceeaf
Cole Gibbs <colegibbs0@gmail.com>
2022-04-18 15:29:46 -0700

    basic structure and pseudo

---
+++ snakes_cafe.py
@@ -0,0 +1,38 @@
+# render dictionaries
+# loop and print functions for the dish lists
+
+print("""$ python snakes_cafe.py
+**************************************
+**    Welcome to the Snakes Cafe!   **
+**    Please see our menu below.    **
+**
+** To quit at any time, type "quit" **
+**************************************""")
+
+print("""Appetizers
+----------""")
+
+#loop function to render appetizers
+
+print("""Entrees
+-------""")
+
+#loop function to render entrees
+
+print("""Desserts
+--------""")
+
+#loop function to render desserts
+
+print("""Drinks
+------""")
+ 
+ #loop function to render drinks
+
+print("""***********************************
+** What would you like to order? **
+***********************************""")
+
+# while loop that handles input 
+  # needs to input("> ")
+  #plural vs non-plural
